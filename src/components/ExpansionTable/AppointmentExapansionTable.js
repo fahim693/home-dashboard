1fbdbbe90a206c28d387996a1486dd9dd176f7ca
Fahim Chowdhury <fahimchowdhury@Fahims-MacBook-Pro.local>
2020-07-10 01:55:11 +0600

    changed expansion table initial index

--- src/components/ExpansionTable/AppointmentExapansionTable.js
+++ src/components/ExpansionTable/AppointmentExapansionTable.js
@@ -389,7 +389,7 @@ export default function AppointmentExpansionTable(props) {
                         <TableBody>
                             {
                                 !expanded ?
-                                    getLimited(4) : getUnlimited()
+                                    getLimited(3) : getUnlimited()
                             }
                         </TableBody>
                     </Table>
