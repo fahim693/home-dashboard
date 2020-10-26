42b2b0f96e660c2cc6867670f36b2a0331e86a5f
Fahim Chowdhury <md.fahim693@gmail.com>
2020-10-26 22:30:31 +0600

    subscription packages with details

--- src/components/Subscription/Packages.js
+++ src/components/Subscription/Packages.js
@@ -1,5 +1,6 @@
 import { Button, Card, CardContent, Grid } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
+import PackageDetails from './PackageDetails';
 
 const packs = [
     {
@@ -19,16 +20,18 @@ const packs = [
     },
 ]
 
+
 const Packages = () => {
+    const [selected, setSelected] = useState(1)
     return (
-        <div style={{ backgroundColor: '#F3F4F6', padding: '50px 60px' }}>
+        <div style={{ backgroundColor: '#F3F4F6' }}>
             <Grid container spacing={5}>
                 {
                     packs.map((item, idx) => {
                         if (idx === 1)
                             return (
                                 <Grid key={idx} item sm={4} xs={12}>
-                                    <Card style={{ padding: '0 0 34px' }}>
+                                    <Card style={{ border: selected === 1 ? '1px solid #4BCA81' : '1px solid #E0E0E0', padding: '0 0 34px' }}>
                                         <div style={{ color: '#fff', backgroundColor: '#2A95C5', height: 48, fontSize: 18, marginBottom: 29, fontWeight: 500, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                                             Most Popular
                                     </div>
@@ -37,7 +40,17 @@ const Packages = () => {
                                             <div style={{ color: '#4F4F4F', fontSize: 39, marginBottom: 6 }}>${item.price}</div>
                                             <div style={{ color: '#828282', fontSize: 17, marginBottom: 20 }}>per month</div>
                                             <div style={{ color: '#219653', fontSize: 20, marginBottom: 38 }}>{item.user}</div>
-                                            <Button variant='contained' style={{ backgroundColor: '#2A95C5', color: '#fff', width: 230 }}>SELECT PLAN</Button>
+                                            {
+                                                selected === 1 ?
+                                                    <Button variant='contained' style={{ backgroundColor: '#4BCA81', color: '#fff', width: 230 }} startIcon={
+                                                        <svg width="15" height="11" viewBox="0 0 15 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                            <path d="M4.49896 8.5013L1.58229 5.58463C1.25729 5.25963 0.740625 5.25963 0.415625 5.58463C0.090625 5.90963 0.090625 6.4263 0.415625 6.7513L3.90729 10.243C4.23229 10.568 4.75729 10.568 5.08229 10.243L13.9156 1.41796C14.2406 1.09296 14.2406 0.576295 13.9156 0.251295C13.5906 -0.0737045 13.074 -0.0737045 12.749 0.251295L4.49896 8.5013Z" fill="white" />
+                                                        </svg>
+                                                    }>
+                                                        SELECTED
+                                                    </Button> :
+                                                    <Button onClick={() => setSelected(idx)} variant='contained' style={{ backgroundColor: '#2A95C5', color: '#fff', width: 230 }}>SELECT PLAN</Button>
+                                            }
                                         </CardContent>
                                     </Card>
                                 </Grid>
@@ -45,19 +58,32 @@ const Packages = () => {
                         else
                             return (
                                 <Grid key={idx} item sm={4} xs={12}>
-                                    <Card style={{ padding: '29px 0 34px', marginTop: 48 }}>
+                                    <Card style={{ border: idx === selected ? '1px solid #4BCA81' : '1px solid #E0E0E0', padding: '29px 0 34px', marginTop: 48 }}>
                                         <CardContent style={{ alignItems: 'center', display: 'flex', flexDirection: 'column', fontWeight: 500 }}>
                                             <div style={{ fontSize: 20, color: '#2A95C5', marginBottom: 26 }}>{item.name}</div>
                                             <div style={{ color: '#4F4F4F', fontSize: 39, marginBottom: 6 }}>${item.price}</div>
                                             <div style={{ color: '#828282', fontSize: 17, marginBottom: 20 }}>per month</div>
                                             <div style={{ color: '#219653', fontSize: 20, marginBottom: 38 }}>{item.user}</div>
-                                            <Button variant='contained' style={{ backgroundColor: '#2A95C5', color: '#fff', width: 230 }}>SELECT PLAN</Button>
+                                            {
+                                                selected === idx ?
+                                                    <Button variant='contained' style={{ backgroundColor: '#4BCA81', color: '#fff', width: 230 }} startIcon={
+                                                        <svg width="15" height="11" viewBox="0 0 15 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                            <path d="M4.49896 8.5013L1.58229 5.58463C1.25729 5.25963 0.740625 5.25963 0.415625 5.58463C0.090625 5.90963 0.090625 6.4263 0.415625 6.7513L3.90729 10.243C4.23229 10.568 4.75729 10.568 5.08229 10.243L13.9156 1.41796C14.2406 1.09296 14.2406 0.576295 13.9156 0.251295C13.5906 -0.0737045 13.074 -0.0737045 12.749 0.251295L4.49896 8.5013Z" fill="white" />
+                                                        </svg>
+                                                    }>
+                                                        SELECTED
+                                                    </Button> :
+                                                    <Button onClick={() => setSelected(idx)} variant='contained' style={{ backgroundColor: '#2A95C5', color: '#fff', width: 230 }}>SELECT PLAN</Button>
+                                            }
                                         </CardContent>
                                     </Card>
                                 </Grid>
                             )
                     })
                 }
+                <Grid item sm={12}>
+                    <PackageDetails />
+                </Grid>
             </Grid>
         </div>
     )
--- src/components/Subscription/Subscribe.js
+++ src/components/Subscription/Subscribe.js
@@ -9,7 +9,7 @@ const Subscribe = (props) => {
             <div style={{ backgroundColor: '#fff', display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '16px 18px', boxShadow: '0px 2px 6px rgba(0, 0, 0, 0.22)', marginBottom: 1 }}>
                 <div style={{ flex: 1, fontWeight: 500, fontSize: 24, color: '#000' }}>Choose Plan</div>
                 <div className='subscription-switch'>
-                    <div style={{ fontSize: 20, fontWeight: 500, marginRight: 6 }}>Billed Monthly</div>
+                    <div style={{ fontSize: 20, fontWeight: 500, marginRight: 7 }}>Billed Monthly</div>
                     <Switch
                         // checked={state.checkedB}
                         // onChange={handleChange}
@@ -17,7 +17,7 @@ const Subscribe = (props) => {
                         name="checkedB"
                         inputProps={{ 'aria-label': 'primary checkbox' }}
                     />
-                    <div style={{ fontSize: 20, fontWeight: 500, marginLeft: 6 }}>Billed Yearly</div>
+                    <div style={{ fontSize: 20, fontWeight: 500, marginLeft: 7 }}>Billed Yearly</div>
                     <div style={{ marginLeft: 14, backgroundColor: '#F2C94C', height: 30, width: 92, display: 'flex', alignItems: 'center', justifyContent: 'center', borderRadius: 6, fontSize: 16, fontWeight: 500 }}>Save 20%</div>
                 </div>
                 <div style={{ flex: 1, display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
@@ -25,7 +25,9 @@ const Subscribe = (props) => {
                     <Button variant='contained' style={{ backgroundColor: '#2A95C5', color: '#fff' }}>Pay Now</Button>
                 </div>
             </div>
-            <Packages />
+            <div style={{ padding: '40px 60px' }}>
+                <Packages />
+            </div>
         </div >
     )
 }
