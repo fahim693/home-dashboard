fba772a46fcf37bc76b699c265c38b3069ac5c06
Fahim Chowdhury <md.fahim693@gmail.com>
2020-11-20 19:06:55 +0600

    open schedule modal in add new

--- src/components/ScheduleScreen/Schedule.js
+++ src/components/ScheduleScreen/Schedule.js
@@ -4,17 +4,18 @@ import React, { useState } from 'react'
 import { KeyboardDatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import MomentUtils from '@date-io/moment';
 import './schedule.css'
-import { Avatar } from '@material-ui/core';
+import { Avatar, Button } from '@material-ui/core';
 import moment from 'moment';
 import UnassignedUser from './UnassignedUser';
 import GridSlot from './GridSlot';
 import Modal from './Modal'
+import { withRouter } from 'react-router-dom';
 
 
 const halfHourlyTime = ['8:00 AM', '8:30 AM', '9:00 AM', '9:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '1:00 PM', '1:30 PM', '2:00 PM', '2:30 PM', '3:00 PM', '3:30 PM', '4:00 PM', '4:30 PM', '5:00 PM', '5:30 PM', '6:00 PM', '6:30 PM', '7:00 PM', '7:30 PM', '8:00 PM', '8:30 PM', '9:00 PM', '9:30 PM', '10:00 PM', '10:30 PM', '11:00 PM', '11:30 PM', '12:00 AM', '12:30 AM', '1:00 AM', '1:30 AM', '2:00 AM', '2:30 AM', '3:00 AM', '3:30 AM', '4:00 AM', '4:30 AM', '5:00 AM', '5:30 AM', '6:00 AM', '6:30 AM', '7:00 AM', '7:30 AM',]
 const scheduleTime = ['8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM', '7:00 PM', '8:00 PM', '9:00 PM', '10:00 PM', '11:00 PM', '12:00 AM', '1:00 AM', '2:00 AM', '3:00 AM', '4:00 AM', '5:00 AM', '6:00 AM', '7:00 AM']
 const timeOff = [0, 1, 2, 3, 12, 13, 14, 15, 16, 17]
-const Schedule = () => {
+const Schedule = (props) => {
     const [alignment, setAlignment] = React.useState('left');
     const [open, setOpen] = useState(false)
     const [modalText, setModalText] = useState('')
@@ -123,7 +124,7 @@ const Schedule = () => {
         if (type === 0) {
             setModalText('The time you have selected already has an existing appointment. Would you still like to continue?')
             setOpen(true);
-        }else{
+        } else {
             setModalText('The time you have selected is out of business hours. Would you still like to continue?')
             setOpen(true);
         }
@@ -230,76 +231,104 @@ const Schedule = () => {
                     <svg onClick={handleNextDay} style={{ cursor: 'pointer' }} width="9" height="14" viewBox="0 0 9 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M1.16168 0.828341C1.61668 0.373341 2.35168 0.373341 2.80668 0.828341L8.16168 6.18334C8.61668 6.63834 8.61668 7.37334 8.16168 7.82834L2.80668 13.1833C2.35168 13.6383 1.61668 13.6383 1.16168 13.1833C0.706677 12.7283 0.706677 11.9933 1.16168 11.5383L5.68835 7.00001L1.16168 2.47334C0.706677 2.01834 0.718344 1.27167 1.16168 0.828341Z" fill="black" />
                     </svg>
-
                 </div>
                 <div style={{ flex: 1, display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
-                    <ToggleButtonGroup
-                        value={alignment}
-                        exclusive
-                        onChange={handleAlignment}
-                        aria-label="text alignment"
-                    >
-                        <ToggleButton value="left" aria-label="left aligned" style={{ width: 106 }}>
-                            Dispatch
+                    {
+                        props.modal ?
+                            <React.Fragment>
+                                <Button variant='outlined'
+                                    onClick={() => {
+                                        props.history.push(props.link)
+                                        props.setOpen(false);
+                                    }}
+                                    startIcon={
+                                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M7.992 0C3.576 0 0 3.584 0 8C0 12.416 3.576 16 7.992 16C12.416 16 16 12.416 16 8C16 3.584 12.416 0 7.992 0ZM8 14.4C4.464 14.4 1.6 11.536 1.6 8C1.6 4.464 4.464 1.6 8 1.6C11.536 1.6 14.4 4.464 14.4 8C14.4 11.536 11.536 14.4 8 14.4Z" fill="#2A95C5" />
+                                            <path d="M8.25 4H7V8.59016L11.375 11L12 10.059L8.25 8.01639V4Z" fill="#2A95C5" />
+                                        </svg>
+                                    }
+                                    style={{ color: '#2A95C5', border: '2px solid #D6DBE6', marginRight: 22 }}>SCHEDULE LATER</Button>
+                                <Button variant='contained'
+                                    onClick={() => {
+                                        props.setOpen(false);
+                                        props.history.push(props.link)
+                                    }}
+                                    style={{ color: '#fff', backgroundColor: '#2A95C5' }}>SCHEDULE</Button>
+                            </React.Fragment>
+                            :
+                            <React.Fragment>
+                                <ToggleButtonGroup
+                                    value={alignment}
+                                    exclusive
+                                    onChange={handleAlignment}
+                                    aria-label="text alignment"
+                                >
+                                    <ToggleButton value="left" aria-label="left aligned" style={{ width: 106 }}>
+                                        Dispatch
                     </ToggleButton>
-                        <ToggleButton value="center" aria-label="centered" style={{ width: 106 }}>
-                            Day
+                                    <ToggleButton value="center" aria-label="centered" style={{ width: 106 }}>
+                                        Day
                     </ToggleButton>
-                        <ToggleButton value="right" aria-label="right aligned" style={{ width: 106 }}>
-                            Week
+                                    <ToggleButton value="right" aria-label="right aligned" style={{ width: 106 }}>
+                                        Week
                     </ToggleButton>
-                        <ToggleButton value="justify" aria-label="justified" style={{ width: 106 }}>
-                            Month
+                                    <ToggleButton value="justify" aria-label="justified" style={{ width: 106 }}>
+                                        Month
                     </ToggleButton>
-                    </ToggleButtonGroup>
+                                </ToggleButtonGroup>
+                            </React.Fragment>
+                    }
                 </div>
             </div>
-            <div style={{ height: 65, backgroundColor: '#fff', display: 'flex', alignItems: 'center', boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.12)' }}>
-                <div style={{ paddingLeft: 19, fontWeight: 500, fontSize: 18, color: '#828282', width: 194, display: 'flex', alignItems: 'center' }}>
-                    Unscheduled
-                    <div
-                        style={{
-                            padding: 4,
-                            minWidth: 16,
-                            textAlign: 'center',
-                            borderRadius: 6,
-                            fontSize: 17,
-                            color: '#000',
-                            marginLeft: 12,
-                            backgroundColor: '#F3D06D'
-                        }}
-                    >{unassignedUser.length}</div>
-                </div>
-                <div style={{ padding: '0 24px', height: 65, display: 'flex', backgroundColor: '#f7f7f7', alignItems: 'center', flex: 1, overflow: 'hidden' }}>
-                    <div style={{ cursor: 'pointer', marginRight: 18 }}>
-                        <svg width="9" height="14" viewBox="0 0 9 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M8.60023 0.471141C8.0672 -0.157047 7.20615 -0.157047 6.67312 0.471141L0.399772 7.86443C-0.133257 8.49262 -0.133257 9.50738 0.399772 10.1356L6.67312 17.5289C7.20615 18.157 8.0672 18.157 8.60023 17.5289C9.13326 16.9007 9.13326 15.8859 8.60023 15.2577L3.29727 8.99195L8.60023 2.74228C9.13326 2.11409 9.11959 1.08322 8.60023 0.471141Z" fill="black" />
-                        </svg>
-                    </div>
-                    <div style={{ padding: '12px 0px', display: 'flex', overflowX: 'auto', whiteSpace: 'nowrap', flex: 1 }}>
-                        {
-                            unassignedUser.map((item, idx) => {
-                                return (
-                                    <UnassignedUser
-                                        key={idx}
-                                        scheduleTime={scheduleTime}
-                                        handleSelected={handleSelected}
-                                        item={item}
-                                        idx={idx}
-                                        modal={handleModal}
-                                        removeIdx={setRemoveIdx}
-                                    />
-                                )
-                            })
-                        }
-                    </div>
-                    <div style={{ cursor: 'pointer', marginLeft: 18 }}>
-                        <svg width="9" height="14" viewBox="0 0 9 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M0.39977 17.5289C0.9328 18.157 1.79385 18.157 2.32688 17.5289L8.60023 10.1356C9.13326 9.50738 9.13326 8.49262 8.60023 7.86443L2.32688 0.471141C1.79385 -0.157046 0.932802 -0.157046 0.399771 0.471141C-0.133258 1.09933 -0.133258 2.11409 0.399771 2.74228L5.70273 9.00805L0.39977 15.2577C-0.13326 15.8859 -0.119592 16.9168 0.39977 17.5289Z" fill="black" />
-                        </svg>
-                    </div>
-                </div>
-            </div>
+            {
+                !props.modal ?
+                    <div style={{ height: 65, backgroundColor: '#fff', display: 'flex', alignItems: 'center', boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.12)' }}>
+                        <div style={{ paddingLeft: 19, fontWeight: 500, fontSize: 18, color: '#828282', width: 194, display: 'flex', alignItems: 'center' }}>
+                            Unscheduled
+                            <div
+                                style={{
+                                    padding: 4,
+                                    minWidth: 16,
+                                    textAlign: 'center',
+                                    borderRadius: 6,
+                                    fontSize: 17,
+                                    color: '#000',
+                                    marginLeft: 12,
+                                    backgroundColor: '#F3D06D'
+                                }}
+                            >{unassignedUser.length}</div>
+                        </div>
+                        <div style={{ padding: '0 24px', height: 65, display: 'flex', backgroundColor: '#f7f7f7', alignItems: 'center', flex: 1, overflow: 'hidden' }}>
+                            <div style={{ cursor: 'pointer', marginRight: 18 }}>
+                                <svg width="9" height="14" viewBox="0 0 9 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M8.60023 0.471141C8.0672 -0.157047 7.20615 -0.157047 6.67312 0.471141L0.399772 7.86443C-0.133257 8.49262 -0.133257 9.50738 0.399772 10.1356L6.67312 17.5289C7.20615 18.157 8.0672 18.157 8.60023 17.5289C9.13326 16.9007 9.13326 15.8859 8.60023 15.2577L3.29727 8.99195L8.60023 2.74228C9.13326 2.11409 9.11959 1.08322 8.60023 0.471141Z" fill="black" />
+                                </svg>
+                            </div>
+                            <div style={{ padding: '12px 0px', display: 'flex', overflowX: 'auto', whiteSpace: 'nowrap', flex: 1 }}>
+                                {
+                                    unassignedUser.map((item, idx) => {
+                                        return (
+                                            <UnassignedUser
+                                                key={idx}
+                                                scheduleTime={scheduleTime}
+                                                handleSelected={handleSelected}
+                                                item={item}
+                                                idx={idx}
+                                                modal={handleModal}
+                                                removeIdx={setRemoveIdx}
+                                            />
+                                        )
+                                    })
+                                }
+                            </div>
+                            <div style={{ cursor: 'pointer', marginLeft: 18 }}>
+                                <svg width="9" height="14" viewBox="0 0 9 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M0.39977 17.5289C0.9328 18.157 1.79385 18.157 2.32688 17.5289L8.60023 10.1356C9.13326 9.50738 9.13326 8.49262 8.60023 7.86443L2.32688 0.471141C1.79385 -0.157046 0.932802 -0.157046 0.399771 0.471141C-0.133258 1.09933 -0.133258 2.11409 0.399771 2.74228L5.70273 9.00805L0.39977 15.2577C-0.13326 15.8859 -0.119592 16.9168 0.39977 17.5289Z" fill="black" />
+                                </svg>
+                            </div>
+                        </div>
+                    </div> : ''
+            }
             <div style={{ display: 'flex' }}>
                 <div style={{ width: 214 }}>
                     <div style={{ paddingLeft: 19, height: 44, fontWeight: 500, fontSize: 18, color: '#333', backgroundColor: '#fff', boxShadow: '2px 0px 4px rgba(0, 0, 0, 0.12), inset -1px 1px 0px #E0E0E0, inset 0px -1px 0px #D6D6D6', display: 'flex', alignItems: 'center' }}>
@@ -342,4 +371,4 @@ const Schedule = () => {
     )
 }
 
-export default Schedule;
+export default withRouter(Schedule);
