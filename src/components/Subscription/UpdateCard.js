723b7eeddb298394cd8b071e82b1d45d1e392297
Fahim <md.fahim693@gmail.com>
2021-02-02 14:06:27 +0600

    update credit card pop up

--- src/components/Subscription/UpdateCard.js
+++ src/components/Subscription/UpdateCard.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
-import { Button, TextField } from '@material-ui/core';
+import { Button, FormControl, TextField } from '@material-ui/core';
 import InputMask from 'react-input-mask';
 
 const useStyles = makeStyles((theme) => ({
@@ -11,10 +11,28 @@ const useStyles = makeStyles((theme) => ({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    textInput: {
+        "& .MuiSelect-select": {
+            '&:focus': {
+                backgroundColor: '#fff',
+            }
+        },
+        "& .MuiOutlinedInput-notchedOutline": {
+            border: "2px solid #EB5758"
+        },
+        "&:hover .MuiOutlinedInput-notchedOutline": {
+            border: "1px solid #000"
+        },
+        "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
+            border: "2px solid #EB5758",
+            // backgroundColor: "#fff"
+        },
+    }
 }));
 
 export default function UpdateCardModal(props) {
     const classes = useStyles();
+    const [invalidWarning, setInvalidWarning] = useState(false)
     const [cardInfo, setCardInfo] = useState({
         name: '',
         number: '',
@@ -24,19 +42,16 @@ export default function UpdateCardModal(props) {
     })
 
     const handleChangeCardInfo = (e) => {
+        if (e.target.name === 'name' && e.target.value === ' ') {
+            setInvalidWarning(true)
+        } else {
+            setInvalidWarning(false)
+        }
         setCardInfo({
             ...cardInfo,
             [e.target.name]: e.target.value
         })
     }
-
-    // useEffect(() => {
-    //     const func = () => {
-    //         if (page === 2)
-    //             props.setExists(true);
-    //     }
-    //     func();
-    // }, [page, props])
 
     return (
         <div>
@@ -63,7 +78,7 @@ export default function UpdateCardModal(props) {
                             onClick={() => {
                                 props.setOpen(false)
                             }}
-                            style={{ border: '2px solid #2A95C5', color: '#2A95C5', marginRight: 24 }}>Cancel</Button>
+                            style={{ border: '2px solid #2A95C5', color: '#2A95C5', marginRight: 16 }}>Cancel</Button>
                         <Button variant='contained'
                             onClick={() => props.setOpen(false)}
                             style={{ backgroundColor: '#2A95C5', color: '#fff' }}>Update card</Button>
@@ -73,8 +88,30 @@ export default function UpdateCardModal(props) {
                             <div style={{ fontSize: 28, fontWeight: 500, marginBottom: 34, }}>Credit Card Information</div>
                         </div>
                         <div style={{ fontWeight: 500, fontSize: 20, color: '#333' }}>
-                            <div>Name on card</div>
-                            <TextField style={{ width: 380, marginTop: 8 }} variant="outlined" size='small' name="name" placeholder="Enter Name" value={cardInfo.name} onChange={handleChangeCardInfo} />
+                            <div>Name on card<span style={{ color: '#EB5758' }}>*</span></div>
+                            <FormControl classes={{
+                                root: invalidWarning ? classes.textInput : null
+                            }}>
+                                <TextField
+                                    style={{ width: 380, marginTop: 8 }}
+                                    variant="outlined"
+                                    size='small'
+                                    name="name"
+                                    placeholder="Name on Card"
+                                    value={cardInfo.name}
+                                    onChange={handleChangeCardInfo}
+                                    InputProps={{
+                                        endAdornment:
+                                            invalidWarning ?
+                                                <div className='login-field-warning'>
+                                                    <svg style={{ marginRight: 6 }} width="22" height="19" viewBox="0 0 22 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <path d="M1.72992 19H20.2599C21.0299 19 21.5099 18.17 21.1299 17.5L11.8599 1.5C11.4699 0.829998 10.5099 0.829998 10.1299 1.5L0.859922 17.5C0.479922 18.17 0.959922 19 1.72992 19ZM11.9999 16H9.99992V14H11.9999V16ZM10.9999 12C10.4499 12 9.99992 11.55 9.99992 11V9C9.99992 8.45 10.4499 8 10.9999 8C11.5499 8 11.9999 8.45 11.9999 9V11C11.9999 11.55 11.5499 12 10.9999 12Z" fill="#EB5757" />
+                                                    </svg>
+                                                    <div>Invalid Email</div>
+                                                </div> : ''
+                                    }}
+                                />
+                            </FormControl>
                             <br />
                             <br />
                             <div>Card Number</div>
